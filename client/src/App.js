c258a5986b2692a2e797f5a864f505e48439a18e
Andrew Blyth <andrewblyth90@gmail.com>
2019-09-23 21:31:06 +0100

    Beginning to implement Redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import AppNavBar from "./components/AppNavbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ShoppingList from "./components/ShoppingList";
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render(){
   return (
+    <Provider store={store}>
     <div className="App">
       <AppNavBar />
+      <ShoppingList />
     </div>
+    </Provider>  
   );
   }
 }
